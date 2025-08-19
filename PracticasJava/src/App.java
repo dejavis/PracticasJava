8a1637bff427ae24ca2168df2277c0dee3c66187
dejavis <misdatosdeseguridadkey@gmail.com>
2025-08-19 13:07:48 -0600

    Entrada de datos usando JOptionPane

--- PracticasJava/src/App.java
+++ PracticasJava/src/App.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //int a,b;
+        String a=JOptionPane.showInputDialog(null,"Introduce primer numero: ");
+        int numa = Integer.parseInt(a);
+        String b=JOptionPane.showInputDialog(null,"Introduce otro numero: ");
+        int numb = Integer.parseInt(b);
+        JOptionPane.showMessageDialog(null, "La suma es: "+(numa+numb));
     }
 }
